d2337d15d305026827b204b8b7550966f0c0a9d8
wyzy.andre <andre.braasil@gmail.com>
2020-05-17 18:59:51 -0300

    oops

--- examples/GAN/DCGAN.py
+++ examples/GAN/DCGAN.py
@@ -166,7 +166,7 @@ def get_data():
     assert args.data
     imgs = glob.glob(args.data + '/*.png')
     print(imgs)
-    ds = ImageFromFile(imgs, channel=3, shuffle=True,resize=True)
+    ds = ImageFromFile(imgs, channel=3, shuffle=True,resize=512)
     ds = AugmentImageComponent(ds, get_augmentors())
     ds = BatchData(ds, args.batch)
     ds = MultiProcessRunnerZMQ(ds, 5)
